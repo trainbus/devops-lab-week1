17979999ce39c1d8924799f447b30ebf67dfd20d
Onwuachi <donwuachi@yahoo.com>
2025-10-21 13:39:13 -0500

    fix: pass EC2 IP as argument to web control scripts

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+EC2_PUBLIC_IP=$1
 echo "Starting HAProxy and Nginx on Onwuachi web server..."
-ssh -i ~/.ssh/onwua_key.pem ubuntu@<EC2_PUBLIC_IP> "sudo systemctl start haproxy nginx"
-echo "✅ Web services started successfully."
+ssh -i ~/.ssh/onwua_key.pem ubuntu@${EC2_PUBLIC_IP} "sudo systemctl start haproxy nginx"
+echo "Web services started successfully."
+
+
