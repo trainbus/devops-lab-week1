4e16708dc40c50fb4e8bd4fd5d52723743746f0e
Onwuachi <donwuachi@yahoo.com>
2025-10-23 05:17:48 -0500

    global updates to test

--- scripts/stop-web.sh
+++ scripts/stop-web.sh
@@ -2,11 +2,19 @@
 set -e
 
 EC2_PUBLIC_IP=$1
+SSH_KEY="~/.ssh/onwua_key.pem"
+
 if [ -z "$EC2_PUBLIC_IP" ]; then
   echo "Usage: $0 <EC2_PUBLIC_IP>"
   exit 1
 fi
 
-echo "Stopping HAProxy and Nginx on Onwuachi web server ($EC2_PUBLIC_IP)..."
-ssh -i ~/.ssh/onwua_key.pem ubuntu@${EC2_PUBLIC_IP} "sudo systemctl stop haproxy nginx"
-echo "🛑 Web services stopped successfully."
+echo "🛑 Stopping HAProxy and Nginx on Onwuachi web server ($EC2_PUBLIC_IP)..."
+
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/onwua_key.pem ubuntu@"$EC2_PUBLIC_IP" <<'EOF'
+  set -e
+  sudo systemctl stop haproxy nginx
+  echo "→ Services stopped."
+EOF
+
+echo "🧊 Web services stopped successfully."
