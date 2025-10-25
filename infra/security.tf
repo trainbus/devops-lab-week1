4b2b60488caff8839464c8739dd4e574f75bbfa9
Onwuachi <donwuachi@yahoo.com>
2025-10-25 11:17:18 -0500

    added security.tf to terraform

---
+++ infra/security.tf
@@ -0,0 +1,46 @@
+# Allow SSH from your local IP
+resource "aws_security_group_rule" "ssh_local" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["47.12.88.38/32"]  # replace with your home IP
+  security_group_id = data.aws_security_group.app_sg.id
+  description       = "Allow SSH from Derrick's local machine"
+}
+
+# Allow SSH from GitHub Actions runners
+resource "aws_security_group_rule" "ssh_github_actions" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = [
+    "4.148.0.0/16",
+    "4.149.0.0/18",
+    "4.149.64.0/19",
+    "4.149.96.0/19",
+    "4.149.128.0/17",
+    "4.150.0.0/18",
+    "4.150.64.0/18",
+    "4.150.128.0/18",
+    "4.150.192.0/19",
+    "4.150.224.0/19",
+    "4.151.0.0/18",
+    "4.151.64.0/19"
+  ]
+  security_group_id = data.aws_security_group.app_sg.id
+  description       = "Allow SSH from GitHub Actions runners"
+}
+
+# Allow HTTP (80) and HTTPS (443) for web access
+resource "aws_security_group_rule" "web_access" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_security_group.app_sg.id
+  description       = "Allow public HTTP and HTTPS access"
+}
+
